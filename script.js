8047100271af7aa6d1def1b767cbf7803c0f75aa
noa <noa.menjikoff@gmail.com>
2023-04-05 13:55:34 +0200

    ss

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ function deroulant(){
     b.classList.toggle('animation');
     d.classList.toggle('cacher');
 }
+
+function afficherProfil(){
+    const profil = document.querySelector(".profil");
+    const competence = document.querySelector(".competence");
+    const projets = document.querySelector(".projets");
+    profil.classList.toggle('afficher');
+    competence.classList.toggle('.cacherautre');
+    projets.classList.toggle('.cacherautre');
+}
+
+function afficherCompetence(){
+    const profil = document.querySelector(".profil");
+    const competence = document.querySelector(".competence");
+    const projets = document.querySelector(".projets");
+    if(profil.classList.contains("afficher")){
+        profil.classList.toggle('afficher');
+    }
+    if (competence.classList.contains("cacherautre")){
+        competence.classList.toggle('.cacherautre');
+    }
+    profil.classList.toggle('cacherautre');
+    competence.classList.toggle('.afficher');
+    projets.classList.toggle('.cacherautre');
+}
+
+function afficherProjets(){
+    const profil = document.querySelector(".profil");
+    const competence = document.querySelector(".competence");
+    const projets = document.querySelector(".projets");
+    profil.classList.toggle('cacherautre');
+    competence.classList.toggle('.cacherautre');
+    projets.classList.toggle('.afficher');
+}
